54c008723fe363976b4f237b13a26930cc26405d
ArchLuke <lkong@bu.edu>
2020-03-11 20:45:14 -0400

    Stupid Xft has no documentation so now I am stuck

--- TerminalWm.c
+++ TerminalWm.c
@@ -11,11 +11,30 @@ struct Screen
 	int screen_num;
 	int s_width, s_height; //display_width and height
 }
+struct Win
+{
+	Window win;
+	Atom tag_number; /*the number that appears on top of each window*/
+	Atom mode;/*the mode that a window is under*/
+	Atom status;/*---only in cursorless mode---the master/stack status of a window*/
+	int width, height;
+	GC gc;
+	XftColor scheme[2];
+	XftFont *font;
+	
+}
+struct {Foreground, Background};
 typedef struct Screen Screen;
+typedef struct Win Win;
 Screen *screen;
+int mode;
 /*function prototypes*/
 void init();
 void eventLoop();
+void drawBar(int mode);
+static XftFont *xftFontAlloc(Screen *screen, const char *fontname);
+void xftColorAlloc(Screen *screen, XftColor *color, const char *colorname);
+
 void buttonPress(XEvent *);
 void configureRequest(XEvent *);
 void configureNotify(XEvent *);
@@ -43,6 +62,53 @@ static void (*handler[event]) (XEvent *) = {
 	[MotionNotify] = motionNotify,
 	[UnmapNotify] = unmapNotify
 };
+static XftFont *
+xftFontAlloc(Screen *screen, const char *fontname)
+{
+	XftFont *font = NULL;
+
+	if (fontname && screen) {
+			if (!(xfont = XftFontOpenName(screen->dpy, screen->screen_num, fontname))) {
+			fprintf(stderr, "error with name '%s'\n", fontname);
+			return NULL;
+		}
+	}
+	return xfont;
+}
+void
+xftCollorAlloc(Screen *screen, XftColor *color, const char *colorName)
+{
+	if (!screen || !color || !colorname)
+		return;
+
+	if (!XftColorAllocName(screen->dpy, DefaultVisual(screen->dpy, screen->screen_num),
+	                       DefaultColormap(screen->dpy, screen->screen_num),
+	                       colorname, color))
+		fprintf(stderr,"error, cannot allocate color '%s'", colorName);
+}
+void
+drawRect(Screen *screen, Win *win,GC gc, int x, int y, unsigned int w, unsigned int h, int filled)
+{
+	if (!win)
+		return;
+	if (filled)
+		XFillRectangle(screen->dpy, win->win, gc, x, y, w, h);
+	else
+		XDrawRectangle(screen->dpy, win->win, gc, x, y, w - 1, h - 1);
+}
+void drawText(Screen *screen, Win *win, int x, int y, const char *text)
+{
+	if(!win || !screen)
+		return;
+	XGlyphInfo *glyph=NULL;
+	XftDraw *d= XftDrawCreate(screen->dpy, Win->win,
+		                  DefaultVisual(screen->dpy, screen->screen_num),
+		                  DefaultColormap(screen->dpy, screen->screen_num));
+	XftTextExtentsUtf8(d, win->font, (const FcChar8 *)text, strlen(text), &glyph);
+	XftColor *col=win->scheme[Foreground];
+	XftDrawStringUtf8(d,col,win->font, , ty, (XftChar8 *)buf, len);
+		
+}
 void setup()
 {
 	screen->dpy=XOpenDisplay(NULL);
@@ -58,6 +124,46 @@ void eventloop()
 	for(;;){
 		handler[ev.type](&ev);
 	}
+}
+void drawBar(int mode)
+{
+	GC 
+}
+void buttonPress(XEvent *ev)
+{
+}
+void configureRequest(XEvent *ev)
+{
+}
+void configureNotify(XEvent *ev)
+{
+}
+void destroyNotify(XEvent *ev)
+{
+}
+void enterNotify(XEvent *ev)
+{
+}
+void expose(XEvent *ev)
+{
+}
+void focusIn(XEvent *ev)
+{
+}
+void keyPress(XEvent *ev)
+{
+}
+void mappingNotify(XEvent *ev)
+{
+}
+void mapRequest(XEvent *ev)
+{
+}
+void motionNotify(XEvent *ev)
+{
+}
+void unmapNotify(XEvent *ev)
+{
 }
 int main()
 {
