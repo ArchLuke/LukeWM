20d47b2875202af8c8e8961b6e481ea9f3d14e54
ArchLuke <lkong@bu.edu>
2020-03-11 22:28:32 -0400

    the code is now compilable

--- TerminalWm.c
+++ TerminalWm.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 #include <X11/Xlib.h>
 #include <X11/Xatom.h>
-
+#include <X11/Xft/Xft.h>
 #include "config.h"
 /*data types*/
-struct Screen
+
+
+typedef struct MyScreen MyScreen;
+typedef struct Win Win;
+struct MyScreen
 {
 	Display *dpy;	
 	Window rootwin;
 	int screen_num;
 	int s_width, s_height; //display_width and height
-}
+};
 struct Win
 {
 	Window win;
@@ -22,19 +26,19 @@ struct Win
 	XftColor scheme[2];
 	XftFont *font;
 	
-}
-struct {Foreground, Background};
-typedef struct Screen Screen;
-typedef struct Win Win;
-Screen *screen;
+};
+enum {Foreground, Background};
+
+MyScreen *screen;
 int mode;
 /*function prototypes*/
 void init();
 void eventLoop();
 void drawBar(int mode);
-static XftFont *xftFontAlloc(Screen *screen, const char *fontname);
-void xftColorAlloc(Screen *screen, XftColor *color, const char *colorname);
-
+static XftFont *xft_font_alloc(MyScreen *screen, const char *fontname);
+void xft_color_alloc(MyScreen *screen, XftColor *color, const char *colorname);
+void drawRect(MyScreen *screen, Win *win,GC gc, int x, int y, unsigned int w, unsigned int h, int filled);
+void drawTextCenter(MyScreen *screen, Win *win, const char *text);
 void buttonPress(XEvent *);
 void configureRequest(XEvent *);
 void configureNotify(XEvent *);
@@ -47,8 +51,8 @@ void mappingNotify(XEvent *);
 void mapRequest(XEvent *);
 void motionNotify(XEvent *);
 void unmapNotify(XEvent *);
-/*function array initializer*/
-static void (*handler[event]) (XEvent *) = {
+/*function designated array initializer*/
+static void (*handler[LASTEvent]) (XEvent *) = {
 	[ButtonPress] = buttonPress,
 	[ConfigureRequest] = configureRequest,
 	[ConfigureNotify] = configureNotify,
@@ -63,31 +67,31 @@ static void (*handler[event]) (XEvent *) = {
 	[UnmapNotify] = unmapNotify
 };
 static XftFont *
-xftFontAlloc(Screen *screen, const char *fontname)
+xft_font_alloc(MyScreen *screen, const char *fontname)
 {
 	XftFont *font = NULL;
 
 	if (fontname && screen) {
-			if (!(xfont = XftFontOpenName(screen->dpy, screen->screen_num, fontname))) {
+			if (!(font = XftFontOpenName(screen->dpy, screen->screen_num, fontname))) {
 			fprintf(stderr, "error with name '%s'\n", fontname);
 			return NULL;
 		}
 	}
-	return xfont;
+	return font;
 }
 void
-xftCollorAlloc(Screen *screen, XftColor *color, const char *colorName)
+xft_color_alloc(MyScreen *screen, XftColor *color, const char *colorName)
 {
-	if (!screen || !color || !colorname)
+	if (!screen || !color || !colorName)
 		return;
 
 	if (!XftColorAllocName(screen->dpy, DefaultVisual(screen->dpy, screen->screen_num),
 	                       DefaultColormap(screen->dpy, screen->screen_num),
-	                       colorname, color))
+	                       colorName, color))
 		fprintf(stderr,"error, cannot allocate color '%s'", colorName);
 }
 void
-drawRect(Screen *screen, Win *win,GC gc, int x, int y, unsigned int w, unsigned int h, int filled)
+drawRect(MyScreen *screen, Win *win,GC gc, int x, int y, unsigned int w, unsigned int h, int filled)
 {
 	if (!win)
 		return;
@@ -96,38 +100,48 @@ drawRect(Screen *screen, Win *win,GC gc, int x, int y, unsigned int w, unsigned 
 	else
 		XDrawRectangle(screen->dpy, win->win, gc, x, y, w - 1, h - 1);
 }
-void drawText(Screen *screen, Win *win, int x, int y, const char *text)
+void drawTextCenter(MyScreen *screen, Win *win, const char *text)
 {
 	if(!win || !screen)
 		return;
+
 	XGlyphInfo *glyph=NULL;
-	XftDraw *d= XftDrawCreate(screen->dpy, Win->win,
+	XftDraw *d= XftDrawCreate(screen->dpy, win->win,
 		                  DefaultVisual(screen->dpy, screen->screen_num),
 		                  DefaultColormap(screen->dpy, screen->screen_num));
-	XftTextExtentsUtf8(d, win->font, (const FcChar8 *)text, strlen(text), &glyph);
-	XftColor *col=win->scheme[Foreground];
-	XftDrawStringUtf8(d,col,win->font, , ty, (XftChar8 *)buf, len);
+
+	XftTextExtentsUtf8(screen->dpy, win->font, (const FcChar8 *)text, strlen(text), glyph);
+	XftColor col=win->scheme[Foreground];
+	int width=(win->width-glyph->width)/2;
+	int height=(win->height-glyph->height)/2;
+	XftDrawStringUtf8(d,&col,win->font, width,height,( XftChar8 *)text, strlen(text));
 		
 }
-void setup()
+void init()
 {
 	screen->dpy=XOpenDisplay(NULL);
 	screen->screen_num=DefaultScreen(screen->dpy);
 	screen->rootwin=RootWindow(screen->dpy, screen->screen_num);
-	screen->s_with=DisplayWidth(screen->dpy, screen->screen_num);
+	screen->s_width=DisplayWidth(screen->dpy, screen->screen_num);
 	screen->s_height=DisplayHeight(screen->dpy, screen->screen_num);
-	XSelectInput(screen->dpy, screen->rootWin, SubstructureRedirectMask|SubstructureNotifyMask);	
+	XSelectInput(screen->dpy, screen->rootwin, SubstructureRedirectMask|SubstructureNotifyMask);
+
+	drawBar(mode);	
 }
 void eventloop()
 {
 	XEvent ev;
+	XNextEvent(screen->dpy, &ev);
 	for(;;){
 		handler[ev.type](&ev);
 	}
 }
 void drawBar(int mode)
 {
-	GC 
+	Window bar=XCreateSimpleWindow(screen->dpy, screen->rootwin,0,0, screen->s_width, 15, 1,BlackPixel(screen->dpy,
+			 screen->screen_num),WhitePixel(screen->dpy, screen->screen_num));
+	XMapWindow(screen->dpy, bar);
+	
 }
 void buttonPress(XEvent *ev)
 {
